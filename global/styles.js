15941013b68f2e6cddce687039401013839b18d8
Olha Halat <olha.halat@gmail.com>
2021-05-17 14:05:17 +0200

    create global styles file and create colors

---
+++ global/styles.js
@@ -0,0 +1,17 @@
+export const colors = {
+  // purple: main buttons and highlights
+  primary: {
+    dark: '#5F41EE',
+    light: '#DBD3FF', // used in purple gradients
+  },
+  // peach: gradient backgrounds and secondary highlights
+  secondary: {
+    dark: '#FF7D1C',
+    light: '#FAA96C', // used in gradient background
+  },
+  neutralBackground: '#F6F1F1', // used in gradient background or instead of background grey
+  white: '#FFFFFF',
+  black: '#000000',
+  almostBlack: '#111111', // prefer this for text color instead of black?
+  textFaded: '#767676',
+};
