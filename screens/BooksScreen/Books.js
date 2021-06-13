11c09e2de6aefe084597d868a94f87ae29395289
Olha Halat <olha.halat@gmail.com>
2021-06-13 14:52:16 +0200

    add loading to books.js

--- screens/BooksScreen/Books.js
+++ screens/BooksScreen/Books.js
@@ -18,21 +18,25 @@ import { removeBookFromPool } from '../../redux/actions/poolOfBooksActions';
 import { createMatch } from '../../redux/actions/matchesActions';
 import { getBooksToOffer } from '../../redux/actions/usersBooksActions';
 import { useDispatch, useSelector } from 'react-redux';
+import LoadingModal from '../../components/AlertModal/LoadingModal';
 
 const Books = ({ navigation }) => {
   const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user.user);
   const userToken = useSelector((state) => state.token.token);
-  // console.log({ user });
 
   useEffect(() => {
-    if (user) {
-      dispatch(getPoolOfBooks({ userID: user._id }, userToken));
-      dispatch(getSavedBooks(user.booksToRemember));
-      dispatch(getBooksToOffer(user.booksToOffer));
+    setLoading(true);
+    dispatch(getPoolOfBooks({ userID: user._id }, userToken));
+    dispatch(getSavedBooks(user.booksToRemember));
+    dispatch(getBooksToOffer(user.booksToOffer));
+
+    if (userToken) {
+      setLoading(false);
     }
-  }, [user, userToken, dispatch]);
+  }, []);
 
   const books = useSelector((state) => state.poolOfBooks.books);
   const booksToOffer = useSelector((state) => state.usersBooks.booksToOffer);
@@ -64,98 +68,99 @@ const Books = ({ navigation }) => {
     setShowModal(false);
   };
   return (
-    <SafeAreaView style={styles.container}>
-      <ScreenGradient>
-        <AlertModal
-          showModal={showModal}
-          setShowModal={setShowModal}
-          buttonText="Add a book"
-          whiteButtonText="Not now"
-          handlePress={handlePress}
-        >
-          <PrimaryMedium
-            customStyles={styles.modalText}
-            text="You need to contribute mate!"
-          />
-          <PrimaryText
-            customStyles={styles.modalText}
-            text="One hand washes the other, this is how we do it."
-          />
-          <PrimaryText
-            customStyles={styles.modalText}
-            text="Upload a book — and have a look!"
-          />
-        </AlertModal>
+    // <SafeAreaView style={styles.container}>
+    <ScreenGradient>
+      <LoadingModal showModal={loading} setShowModal={setLoading} />
+      <AlertModal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        buttonText="Add a book"
+        whiteButtonText="Not now"
+        handlePress={handlePress}
+      >
+        <PrimaryMedium
+          customStyles={styles.modalText}
+          text="You need to contribute mate!"
+        />
+        <PrimaryText
+          customStyles={styles.modalText}
+          text="One hand washes the other, this is how we do it."
+        />
+        <PrimaryText
+          customStyles={styles.modalText}
+          text="Upload a book — and have a look!"
+        />
+      </AlertModal>
 
-        {books === undefined || books.length < 1 ? (
-          <NoBookCard navigation={navigation} />
-        ) : (
-          <Swiper
-            cards={books}
-            renderCard={(book) =>
-              book &&
-              book._id && <SwipingBook item={book} navigation={navigation} />
-            }
-            onSwipedLeft={(cardIndex) => handleNope(cardIndex)}
-            onSwipedBottom={(cardIndex) => handleSave(cardIndex)}
-            onSwipedRight={(cardIndex) => handleYes(cardIndex)}
-            cardIndex={0}
-            key={books.length}
-            disableTopSwipe
-            overlayLabelsOpacity
-            infinite
-            backgroundColor={'transparent'}
-            overlayLabels={{
-              left: {
-                title: (
-                  <Feather
-                    name="x-circle"
-                    size={50}
-                    color={colors.error}
-                    backgroundColor={colors.green}
-                  />
-                ),
-                style: {
-                  wrapper: {
-                    alignItems: 'flex-end',
-                    marginTop: 20,
-                    marginLeft: 20,
-                    elevation: 4,
-                  },
+      {books === undefined || books.length < 1 ? (
+        <NoBookCard navigation={navigation} />
+      ) : (
+        <Swiper
+          cards={books}
+          renderCard={(book) =>
+            book &&
+            book._id && <SwipingBook item={book} navigation={navigation} />
+          }
+          onSwipedLeft={(cardIndex) => handleNope(cardIndex)}
+          onSwipedBottom={(cardIndex) => handleSave(cardIndex)}
+          onSwipedRight={(cardIndex) => handleYes(cardIndex)}
+          cardIndex={0}
+          key={books.length}
+          disableTopSwipe
+          overlayLabelsOpacity
+          infinite
+          backgroundColor={'transparent'}
+          overlayLabels={{
+            left: {
+              title: (
+                <Feather
+                  name="x-circle"
+                  size={50}
+                  color={colors.error}
+                  backgroundColor={colors.green}
+                />
+              ),
+              style: {
+                wrapper: {
+                  alignItems: 'flex-end',
+                  marginTop: 20,
+                  marginLeft: 20,
+                  elevation: 4,
                 },
               },
-              right: {
-                title: <Feather name="check-circle" size={50} color="green" />,
-                style: {
-                  wrapper: {
-                    alignItems: 'flex-start',
-                    marginTop: 20,
-                    marginLeft: -20,
-                    elevation: 4,
-                  },
+            },
+            right: {
+              title: <Feather name="check-circle" size={50} color="green" />,
+              style: {
+                wrapper: {
+                  alignItems: 'flex-start',
+                  marginTop: 20,
+                  marginLeft: -20,
+                  elevation: 4,
                 },
               },
-              bottom: {
-                title: (
-                  <Feather
-                    name="bookmark"
-                    size={50}
-                    color={colors.secondary.dark}
-                  />
-                ),
-                style: {
-                  wrapper: {
-                    alignItems: 'center',
-                    marginTop: -80,
-                    elevation: 4,
-                  },
+            },
+            bottom: {
+              title: (
+                <Feather
+                  name="bookmark"
+                  size={50}
+                  color={colors.secondary.dark}
+                />
+              ),
+              style: {
+                wrapper: {
+                  alignItems: 'center',
+                  marginTop: -80,
+                  elevation: 4,
                 },
               },
-            }}
-          />
-        )}
-      </ScreenGradient>
-    </SafeAreaView>
+            },
+          }}
+        />
+      )}
+    </ScreenGradient>
+    // </SafeAreaView>
   );
 };
 
